b3e55fa16c0547af2085065aa8282ee27983c950
Robert Valmassoi <valmassoi@protonmail.com>
2019-09-07 14:16:34 -0400

    init react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import thunk from 'redux-thunk';
 import { compose, createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import './App.css';
 
 import { rootReducer } from './reducers/createStore';
+import Home from './components/Home/Home';
+
 const middleware = [thunk];
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -17,7 +20,9 @@ const App = () => {
   return (
     <div className="App">
       <Provider store={store}>
-        Hello world!
+        <Router>
+          <Route exact path="/" component={Home} />
+        </Router>
       </Provider>
     </div>
   );
